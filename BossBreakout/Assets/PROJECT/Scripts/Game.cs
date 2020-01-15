e1bcf5a8ad4d08ff1fbb4e0a57a540951b2df440
StijnOnline <stijnvandeijzen@hotmail.nl>
2020-01-15 17:18:58 +0100

    Gameover

---
+++ BossBreakout/Assets/PROJECT/Scripts/Game.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviour
+{
+    private bool playing = false;
+    public GameObject canvas;
+    public GameObject ball;
+
+    public void Update() {
+        if(!playing && Input.anyKey) {
+            Play();
+        }
+    }
+
+    public void Play() {
+        playing = true;
+        canvas.SetActive(false);
+        ball.SetActive(true);
+    }
+
+    public void GameOver(GameObject g, Collider2D c) {
+        Debug.Log("GAMEOVER");
+        playing = false;
+        canvas.SetActive(true);
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ BossBreakout/Assets/PROJECT/Scripts/TriggerEvent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class MyTriggerEvent : UnityEvent<GameObject, Collider> { }
+[System.Serializable]
+public class MyTriggerEvent2D : UnityEvent<GameObject, Collider2D> { }
+
+public class TriggerEvent : MonoBehaviour
+{
+    public MyTriggerEvent triggerEvent = new MyTriggerEvent();
+    public MyTriggerEvent2D triggerEvent2D = new MyTriggerEvent2D();
+
+    private void OnTriggerEnter(Collider other) {
+        triggerEvent.Invoke(gameObject, other);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        triggerEvent2D.Invoke(gameObject, other);
+    }
+}
